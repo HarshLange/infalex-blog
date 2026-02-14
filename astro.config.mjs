16ee40a7524fc8bd415666a2df946c4965ae3d95
Harsh Lange <harshlange3@gmail.com>
2026-02-15 01:43:34 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog.infalex.com',
   integrations: [mdx(), sitemap()],
 
   vite: {
